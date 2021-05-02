75d2339b89cbb3b9747ab0a6c47cf1f8eaf81697
TaeKyeong Wang <bugiii@gmail.com>
2021-05-02 18:42:43 +0900

    BLE permission

--- app/src/main/java/me/autolock/m100/cmi/MainActivity.kt
+++ app/src/main/java/me/autolock/m100/cmi/MainActivity.kt
@@ -1,10 +1,27 @@
 package me.autolock.m100.cmi
 
+import android.Manifest
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import me.autolock.m100.cmi.databinding.ActivityMainBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
+
+// used to identify adding bluetooth names
+const val REQUEST_ENABLE_BT = 1
+// used to request fine location permission
+const val REQUEST_ALL_PERMISSION = 2
+val PERMISSIONS = arrayOf(
+    Manifest.permission.ACCESS_FINE_LOCATION
+)
 
 class MainActivity : AppCompatActivity() {
     private val viewModel by viewModel<MainViewModel>()
@@ -19,6 +36,15 @@ class MainActivity : AppCompatActivity() {
         )
         binding.viewModel = viewModel
 
+        // check if location permission
+        if (!hasPermissions(this, PERMISSIONS)) {
+            requestPermissions(PERMISSIONS, REQUEST_ALL_PERMISSION)
+        }
+
+        initObserver(binding)
+    }
+
+    private fun initObserver(binding: ActivityMainBinding) {
         viewModel.logText.observe(this, {
             binding.logText.append(it)
             if ((binding.logText.measuredHeight - binding.logScroll.scrollY) <= (binding.logScroll.height + binding.logText.lineHeight)) {
@@ -28,4 +54,36 @@ class MainActivity : AppCompatActivity() {
             }
         })
     }
+
+    override fun onResume() {
+        super.onResume()
+        // finish app if the BLE is not supported
+        if (!packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            finish()
+        }
+    }
+
+    private val requestEnableBleResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val intent = result.data
+            // do somthing after enableBleRequest
+        }
+    }
+
+    private fun requestEnableBLE() {
+        val bleEnableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+        requestEnableBleResult.launch(bleEnableIntent)
+    }
+
+    private fun hasPermissions(context: Context?, permissions: Array<String>): Boolean {
+        if (context != null) {
+            for (permission in permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
 }
